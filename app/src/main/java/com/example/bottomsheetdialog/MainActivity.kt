6159d8b9be4a0d1020bddb01f6aa509ecdaa0daf
Ashish <sahashish100@gmail.com>
2023-07-03 13:06:25 +0530

    First Commit

---
+++ app/src/main/java/com/example/bottomsheetdialog/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.bottomsheetdialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.bottomsheet.BottomSheetDialog
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var showButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        showButton = findViewById(R.id.showButton);
+
+        showButton.setOnClickListener {
+
+            val dialog = BottomSheetDialog(this)
+
+            val view = layoutInflater.inflate(R.layout.bottom_sheet_fragment, null)
+
+            val btnClose = view.findViewById<Button>(R.id.cancel)
+
+            btnClose.setOnClickListener {
+                dialog.dismiss()
+            }
+            dialog.setCancelable(false)
+
+            dialog.setContentView(view)
+
+            dialog.show()
+        }
+
+
+    }
+}
